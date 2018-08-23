ba35cee51b94d3877a78cdb1835ecb771faaa1d3
ZuRun's MacBook Pro <hi@zuhr.me>
2018-08-23 09:05:55 +0800

    多线程

---
+++ thread/thread-01/src/main/java/safeend/EndRunnable.java
@@ -0,0 +1,23 @@
+package safeend;
+
+
+/**
+ * @author zurun
+ * @date 2018/8/22 23:40:12
+ */
+public class EndRunnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("Thread is start");
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println(threadName + " is run");
+            }
+            System.out.println(threadName + " interrupt flag is " + Thread.currentThread().isInterrupted());
+        }, "endThreadA");
+
+        thread.start();
+        Thread.sleep(1000L);
+        thread.interrupt();
+    }
+}
---
+++ thread/thread-01/src/main/java/safeend/EndThread.java
@@ -0,0 +1,9 @@
+package safeend;
+
+/**
+ * @author zurun
+ * @date 2018/8/22 23:39:22
+ */
+public class EndThread {
+
+}
