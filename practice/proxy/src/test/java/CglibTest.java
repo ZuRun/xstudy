607fd3760d102c143ecef6dbefc94dade8a8f202
ZuRun's MacBook Pro <hi@zuhr.me>
2018-10-12 23:37:48 +0800

    aqs

--- practice/proxy/src/test/java/CglibTest.java
+++ practice/proxy/src/test/java/CglibTest.java
@@ -1,4 +1,9 @@
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author zurun
@@ -11,4 +16,34 @@ public class CglibTest {
     public void test1() {
         HelloWorldFactory.createObj().sayHello();
     }
+
+    public static void main(String[] args) {
+        A a = new A();
+        a.setName("zurun");
+        a.setData(new HashMap());
+        B b = JSON.parseObject(a.toString(), B.class);
+        System.out.println(b.toString());
+    }
+
+    @Data
+    static class A {
+        private String name;
+        private Map data;
+
+        @Override
+        public String toString() {
+            return JSON.toJSONString(this);
+        }
+    }
+
+    @Data
+    static class B {
+        private String name;
+        private String data;
+        @Override
+        public String toString() {
+            return JSON.toJSONString(this);
+        }
+    }
+
 }
---
+++ thread/thread-04/src/main/java/com/xstyud/test/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author zurun
+ * @date 2018/10/12 23:33:39
+ */
+package com.xstyud.test;
