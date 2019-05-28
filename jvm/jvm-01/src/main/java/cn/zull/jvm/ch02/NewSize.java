60f889fa61668671be9c232098d8b0616e23b4bf
ZuRun's MacBook Pro <hi@zull.cn>
2019-05-28 20:52:55 +0800

    jvm练习

---
+++ jvm/jvm-01/src/main/java/cn/zull/jvm/ch02/NewSize.java
@@ -0,0 +1,31 @@
+package cn.zull.jvm.ch02;
+
+/**
+ * @author zurun
+ * @date 2019-05-25 20:39:52
+ */
+public class NewSize {
+
+    /**
+     * -Xms20M -Xmx20M -XX:+PrintGCDetails
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int cap = 1 * 511 * 1024;
+        for (int i = 0; i < 310; i++) {
+            byte[] b1 = new byte[cap];
+        }
+//        byte[] b1 = new byte[cap];
+//        byte[] b2 = new byte[cap];
+//        byte[] b3 = new byte[cap];
+//        byte[] b4 = new byte[cap];
+//        byte[] b5 = new byte[cap];
+//        byte[] b6 = new byte[cap];
+//        byte[] b7 = new byte[cap];
+//        byte[] b8 = new byte[cap];
+//        byte[] b9 = new byte[cap];
+//        byte[] b10 = new byte[cap];
+
+    }
+}
--- jvm/jvm-01/src/main/java/cn/zull/jvm/oom/ss.java
+++
@@ -1,8 +0,0 @@
-package cn.zull.jvm.oom;
-
-/**
- * @author zurun
- * @date 2019-05-25 20:39:52
- */
-public class ss {
-}
